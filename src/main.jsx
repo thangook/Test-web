e267febca422f5bd81aa15dc892a71bcc7c8d6c9
thangook <thang102bg@gmail.com>
2026-02-26 06:18:53 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// Sửa từ './App.jsx' thành '../App.jsx' vì App.jsx của bạn đang nằm ở thư mục gốc
+import App from '../App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
